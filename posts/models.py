f0c5285c1b7e1bde2f2ae4b662b269bf7603b1b6
Eric Monaro Bianchini <eric.bianchini@usp.br>
2023-11-08 19:06:05 -0300

    criação do model "Post" e migrations dele

--- posts/models.py
+++ posts/models.py
@@ -1,3 +1,12 @@
 from django.db import models
-
+from django.conf import settings
 # Create your models here.
+
+class Post(models.Model):
+    author = models.ForeignKey(settings.AUTH_USER_MODEL,
+                               on_delete=models.CASCADE)
+    text = models.CharField(max_length=255)
+    date = models.DateTimeField()
+    
+    def __str__(self):
+        return f'{self.text} - ({self.post_date}) - ({self.author.username})'
