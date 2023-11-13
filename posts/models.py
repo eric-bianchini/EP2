8b08dd3ae69b98a7649fb546aa652d2d7049bea0
Eric Monaro Bianchini <eric.bianchini@usp.br>
2023-11-12 21:01:28 -0300

    CRUD apenas com views funcionais, sem Django Forms

--- posts/models.py
+++ posts/models.py
@@ -5,8 +5,9 @@ from django.conf import settings
 class Post(models.Model):
     author = models.ForeignKey(settings.AUTH_USER_MODEL,
                                on_delete=models.CASCADE)
+    title = models.CharField(max_length=255, default="Título Padrão")
     text = models.CharField(max_length=255)
     date = models.DateTimeField()
     
     def __str__(self):
-        return f'{self.text} - ({self.post_date}) - ({self.author.username})'
+        return f'{self.title} - ({self.text}) - ({self.date}) - ({self.author.username})'
--- posts/views.py
+++ posts/views.py
@@ -2,38 +2,69 @@ from django.http import HttpResponseRedirect
 from django.urls import reverse
 from .temp_data import post_data
 from django.shortcuts import render
-
+from .models import Post
+from django.shortcuts import render, get_object_or_404
+from datetime import datetime
+from django.contrib.auth.models import User
 
 def detail_post(request, post_id):
-    context = {'post': post_data[post_id - 1]}
+    post = get_object_or_404(Post, pk=post_id)
+    context = {'post': post}
     return render(request, 'posts/detail.html', context)
 
 
 def list_posts(request):
-    context = {"post_list": post_data}
+    post_list = Post.objects.all()
+    context = {"post_list": post_list}
     return render(request, 'posts/index.html', context)
 
 
 def search_posts(request):
     context = {}
     if request.GET.get('query', False):
-        context = {
-            "post_list": [
-                m for m in post_data
-                if request.GET['query'].lower() in m['name'].lower()
-            ]
-        }
+        search_term = request.GET['query'].lower()
+        post_list = Post.objects.filter(text__icontains=search_term)
+        context = {"post_list": post_list}
     return render(request, 'posts/search.html', context)
 
 
 def create_post(request):
     if request.method == 'POST':
-        post_data.append({
-            'name': request.POST['name'],
-            'release_year': request.POST['release_year'],
-            'poster_url': request.POST['poster_url']
-        })
+        post_title = request.POST['title']
+        post_text = request.POST['text']
+        author_id = request.POST['author']
+        post_author = User.objects.get(id=author_id)
+        post = Post(author = post_author, title=post_title, text=post_text,
+                      date = datetime.now())
+        post.save()
         return HttpResponseRedirect(
-            reverse('posts:detail', args=(len(post_data), )))
+            reverse('posts:detail', args=(post.id, )))
     else:
         return render(request, 'posts/create.html', {})
+
+
+def update_post(request, post_id):
+    post = get_object_or_404(Post, pk=post_id)
+
+    if request.method == "POST":
+        post.title = request.POST['title']
+        post.date = datetime.now()
+        post.text = request.POST['text']
+
+        post.save()
+        return HttpResponseRedirect(
+            reverse('posts:detail', args=(post.id, )))
+
+    context = {'post': post}
+    return render(request, 'posts/update.html', context)
+
+
+def delete_post(request, post_id):
+    post = get_object_or_404(Post, pk=post_id)
+
+    if request.method == "POST":
+        post.delete()
+        return HttpResponseRedirect(reverse('posts:index'))
+
+    context = {'post': post}
+    return render(request, 'posts/delete.html', context)
